0b4093ac2d3fc474e2e001780d5b7888accd8a41
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-03 21:23:43 +0700

    Add all Home_work_2

---
+++ Seminar/Seminar_002/Home_work_2/exm_13/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая 
+выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+
+
+Console.WriteLine("Введи число: ");
+long number = Convert.ToInt64(Console.ReadLine());
+string index = Convert.ToString(number);
+if (index.Length > 2)
+    {
+  Console.WriteLine($"Третья цифра: {index[2]}");
+    }
+    else 
+    {
+    Console.WriteLine("-> третьей цифры нет");
+    }
+
+
+
---
+++ Seminar/Seminar_002/Home_work_2/exm_15/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру от 1 до 7, чтобы определить является ли выбранный день недели выходным:");
+string day = Console.ReadLine() ??"";
+
+switch (day)
+{
+    case "1" : 
+    Console.WriteLine("Нет. Понедельник - рабочий день");
+    break;
+
+    case "2" : 
+    Console.WriteLine("Нет. Вторник - рабочий день");
+    break;
+
+    case "3" : 
+    Console.WriteLine("Нет. Среда- рабочий день");
+    break;
+
+    case "4" : 
+    Console.WriteLine("Нет. Четверг - рабочий день");
+    break;
+
+    case "5" : 
+    Console.WriteLine("Нет. Пятница - рабочий день");
+    break;
+
+    case "6" : 
+    Console.WriteLine("Да!!! Суббота - выходной день");
+    break;
+
+    case "7" : 
+    Console.WriteLine("Да!!! Воскресенье - выходной день");
+    break;
+
+    default:
+    Console.WriteLine ("Не выдумывай, у нас всего 7 дней в неделе.");
+    break;
+}   
