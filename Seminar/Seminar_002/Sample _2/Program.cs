7f170b7cd27c18c47f426a2fef4f4d299079908a
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-03 20:08:21 +0700

    Add all sample

---
+++ Seminar/Seminar_002/Sample _2/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите программу, которая выводит случайное 
+трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+
+*/
+
+int GetRandomNumber()
+{
+    return new Random().Next(100, 1000); // 100....999
+}
+
+void ShowRandomNumber(int number)
+{
+    Console.WriteLine($"Случайное 3-х значноечисло: {number}");
+    string str = number.ToString();
+    Console.WriteLine($"> {str[0]}{str[2]}");
+}
+
+ShowRandomNumber(GetRandomNumber()); 
+
+void ShowRandomNumberSecond(int number)
+{
+    Console.WriteLine($"Случайное 3-х значноечисло: {number}");
+    int result = (((number / 100) *10)+ (number %10));
+    Console.WriteLine($"> {result}");
+}
+
+ShowRandomNumberSecond(GetRandomNumber()); 
