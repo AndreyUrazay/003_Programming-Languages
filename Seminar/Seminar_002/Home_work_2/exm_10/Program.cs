138c796184bd07aded0456d90aa6ece3e87b9405
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-03 20:58:01 +0700

    Add Home_work_2/exm_10

---
+++ Seminar/Seminar_002/Home_work_2/exm_10/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает 
+на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите 3х значное число:");
+
+string nstring = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(nstring);
+if ((n >= 100) && (n <= 999))
+{
+    int result = (((n%100)-(n % 10))/10);
+    Console.WriteLine(result);
+}
+else
+    Console.WriteLine("Неверно введено число");
+
