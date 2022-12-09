4af6ff3b4c60ba554c12c5cd4a754c65dbc6fe9d
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-09 20:43:30 +0700

    Add Seminar_003

---
+++ Seminar/Seminar_003/Sample_001/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+        2   |
+            |   1
+----------------------->
+            |
+    3       |   4
+
+*/
+
+/*
+1. reference types (ссылочные типы) - class, char, arrays, list (Хранятся в управляемой куче памяти)
+2. value types (значимые типы) - int, long, double, datetime, struct, enum, decinal (Хранятся на стеке/оперативной памяти)
+*/
+
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result !=0) // TryParse безопасное преобразование из string в int
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+    return result;
+}
+
+int GetQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    return 1;
+    if (x<0 && y>0)
+    return 2;
+    if (x<0 && y<0)
+    return 3;
+
+    return 4;    
+}
+
+int x = GetNumber("Введите координату х=");
+int y = GetNumber("Введите координату y=");
+int quarter = GetQuarter(x, y);
+
+Console.WriteLine($"Точка с координатами ({x}, {y}) лежит в {quarter} четверти");
---
+++ Seminar/Seminar_003/Sample_003/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает
+на вход координаты двух точек и находит
+расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+Console.WriteLine("Введите координаты Х первой точки:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y первой точки:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Х второй точки:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y второй точки:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double DistXY(int x1, int y1, int x2, int y2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)); // Math.Sqrt - корень,  Math.Pow - квадрат
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками = {DistXY(x1, y1, x2, y2)}");
