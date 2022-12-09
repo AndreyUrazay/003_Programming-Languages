e5c4009f32365736cc26d85c824aac3ed4205f16
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-10 02:51:00 +0700

    Home_work_4

---
+++ Seminar/Seminar_004/Home_work_4/Exm_025/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Напишите цикл, который принимает на вход
+ два числа (A и B) и возводит
+ число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        System.Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            System.Console.WriteLine("Некорректный ввод. Введите число");
+        }
+    }
+    return result;
+}
+
+
+int number_a = GetNumber("Введите число, которое возведем в степень:");
+int number_b = GetNumber("Введите число степени:");
+
+
+// Console.WriteLine("Введите число, которое возведем в степень:");
+// int number_a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число степени:");
+// int number_b = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow((number_a),number_b);
+
+Console.WriteLine($"{result} является результатом возведения числа {number_a} в степень {number_b}");
+
+
---
+++ Seminar/Seminar_004/Home_work_4/Exm_027/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая 
+принимает на вход число и выдаёт 
+сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+int SumNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        System.Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            System.Console.WriteLine("Некорректный ввод. Введите число");
+        }
+    }
+    return result;
+}
+
+int number = SumNumber("Введите число для подсчета суммы чисел данного числа: ");
+
+
+int sumNumber = 0;
+
+while (number > 0)
+{
+int numb = number % 10;
+number = number / 10;
+sumNumber = sumNumber + numb;
+}
+
+Console.WriteLine($"Сумма цифр в числе = {sumNumber}");
+
+
