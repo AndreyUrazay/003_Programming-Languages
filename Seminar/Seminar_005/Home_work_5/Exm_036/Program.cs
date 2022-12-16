978edd014748d6eb41b64e5bbb1fc2358dd2dc38
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-17 01:07:03 +0700

    Home_work_5

---
+++ Seminar/Seminar_005/Home_work_5/Exm_036/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+//получить размерность массива с консоли
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите корректное число");
+        }
+    }
+    return result;
+}
+
+//получить заполненный массив рандомными числами
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+
+    return array;
+}
+
+//распечатать массив на консоль
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+int[] array = InitArray(GetNumber("Введите размер массива для генерации чисел: "));
+PrintArray(array);
+
+int sum = 0;
+for (int i = 0; i < array.Length; i += 2)
+    sum = sum + array[i];
+Console.WriteLine($"Всего {array.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+
+
+
---
+++ Seminar/Seminar_005/Home_work_5/Exm_038/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ Задайте массив вещественных чисел. 
+ Найдите разницу между максимальным 
+ и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+//получить размерность массива с консоли
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите корректное число");
+        }
+    }
+    return result;
+}
+
+//получить заполненный массив рандомными числами
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+
+    return array;
+}
+
+//распечатать массив на консоль
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+int[] array = InitArray(GetNumber("Введите размер массива для генерации чисел: "));
+PrintArray(array);
+
+double min_number = Int32.MaxValue;
+double max_number = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max_number)
+        {
+            max_number = array[i];
+        }
+    if (array[i] < min_number)
+        {
+            min_number = array[i];
+        }
+}
+
+Console.WriteLine($"Всего {array.Length} чисел. Максимальное значение = {max_number}, минимальное значение = {min_number}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {(max_number) - (min_number)}");
