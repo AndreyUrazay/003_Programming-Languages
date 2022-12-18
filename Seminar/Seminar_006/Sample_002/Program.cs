e09b818ecc94323a1d65dbbe829e36e47f37bf2d
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-18 14:26:36 +0700

    Seminar_006

---
+++ Seminar/Seminar_006/Sample_002/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа
+ и проверяет, может ли существовать треугольник с 
+ сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона 
+треугольника меньше суммы двух других сторон.
+*/
+
+
+
+int GetNumber(string message)
+{
+    int result;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+    return result;
+}
+
+
+int a = GetNumber("Введите размер 1-ой стороны треугольника:");
+int b = GetNumber("Введите размер 2-ой стороны треугольника:");
+int c = GetNumber("Введите размер 3-ей стороны треугольника:");
+
+bool doCalculation(int a, int b, int c)
+{
+return a < b + c && b < a + c && c < a + b;
+}
+
+
+Console.WriteLine($"Стороны: {a}, {b}, {c}");
+
+string ne = doCalculation(a, b, c) ? "" : " не";
+
+Console.WriteLine($"Треугольник{ne} может существовать");
