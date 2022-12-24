2008175a64506348ebfdde590fbf0af18517ca5f
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-24 21:17:16 +0700

    Home_work_008

---
+++ Seminar/Seminar_008/Home_work_008/Exm_056/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку 
+с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и 
+выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+
+int m = GetNumber("Введите количество строк:");
+int n = GetNumber("Введите количество столбцов:");
+int[,] matrix = new int[m, n];
+FillArrayRandomNumbers(matrix);
+PrintArray(matrix);
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+void FillArrayRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
+
+// Минимальная сумма жлементов в строке
+int MinSummLine = 0;
+int SummLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(matrix, i);
+    if (SummLine > tempSumLine)
+    {
+        SummLine = tempSumLine;
+        MinSummLine = i;
+    }
+}
+
+Console.WriteLine($"\n{MinSummLine + 1} - строка, которая имеет наименьщую сумму элементов. Сумма элементов = {SummLine}");
+
+int SumLineElements(int[,] matrix, int i)
+{
+    int SummLine = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        SummLine += matrix[i, j];
+    }
+    return SummLine;
+}
---
+++ Seminar/Seminar_008/Home_work_008/Exm_058/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+
+*/
+
+int rowsA = GetNumber("Введите количество строк для 1-ой матрицы:");
+int columnsA = GetNumber("Введите количество столбцов для 1-ой матрицы:");
+int[,] matrixA = new int[rowsA, columnsA];
+
+int rowsB = GetNumber("Введите количество строк для 2-ой матрицы:");
+int columnsB = GetNumber("Введите количество столбцов для 2-ой матрицы:");
+int[,] matrixB = new int[rowsB, columnsB];
+
+
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
+}
+
+Console.WriteLine("1-ая матрица имеет следующий вид:");
+FillArrayRandomNumbers(matrixA);
+PrintArray(matrixA);
+
+Console.WriteLine("2-ая матрица имеет следующий вид:");
+FillArrayRandomNumbers(matrixB);
+PrintArray(matrixB);
+
+Console.WriteLine("Матрица произведения:");
+FillArrayRandomNumbers(MultiplicationMatrix(matrixA, matrixB));
+PrintArray(MultiplicationMatrix(matrixA, matrixB));
+
+// проверка корректности ввода числа
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+// функция ввода рандомной матрицы
+void FillArrayRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+// функция отображения сгенерированной матрицы
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
+
+int[,] MultiplicationMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return matrixC;
+}
