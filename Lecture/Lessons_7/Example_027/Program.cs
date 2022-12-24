5f189ab34f6d33f1860537ceec7c99f69bf546db
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-25 02:22:51 +0700

    Lessons_8

---
+++ Lecture/Lessons_7/Example_027/Program.cs
@@ -0,0 +1,31 @@
+﻿// как посмотреть директорию папок. Обход директории
+string path = "H:\GeekBrains\003_Знакомство с языками програмиирования\003_git_PL\Lecture\Lessons_1"
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+
+
+// Рекурсия
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent} {catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent} {files[i].Name}");
+    }
+}
+string path = @"H:\GeekBrains\003_Знакомство с языками програмиирования\003_git_PL\Lecture\Lessons_1";
+CatalogInfo(path);
