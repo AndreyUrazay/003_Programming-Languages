f7459e37dd5a6382d9bdec4a25588173bddacbb1
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-24 18:14:42 +0700

    Home_work_007

---
+++ Seminar/Seminar_007/Home_work_007/Exm_050/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход 
+принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+
+Console.WriteLine("Введите номер строки");
+int rows_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int colums_1 = Convert.ToInt32(Console.ReadLine());
+
+if (rows_1 > matrix.GetLength(0) || colums_1 > matrix.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {rows_1} строки и {colums_1} столбца равно {matrix[rows_1 - 1, colums_1 - 1]}");
+}
+
