478e4a2925ac17f16c306e6abbe9af7b390a3e1a
Andrey Urazay <urazay.andrey@mail.ru>
2023-02-12 19:06:37 +0700

    Control_work добавили вывод и тело программы

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -34,4 +34,46 @@ string[] SelectItem(string[] arr, int selectLength = 3)
         }
         return result;
     }
+}void Print(string[] arr)
+{
+    if (arr.Length == 0) Console.WriteLine("[]");
+    else
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{arr[i]}");
+                if (i == arr.Length-1) Console.Write("]");
+                else Console.Write(", ");
+            }
+            else if (i< arr.Length-1)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            else 
+            {
+                Console.Write($"{arr[i]}]");
+            }
+        }
+        
+    }
 }
+
+void PrintResult(string[] arr, string[] result)
+{
+    Print(arr);
+    Console.Write(" --> ");
+    Print(result);
+    Console.WriteLine();
+}
+string [] array1 = {"hello", "2", ":-)"};
+string [] array2 = {"1234", "1567", "-2", "computer science"};
+string [] array3 = {"Russia", "Demmark", "Kazan"};
+
+PrintResult(array1, SelectItem(array1));
+PrintResult(array2, SelectItem(array2));
+PrintResult(array3, SelectItem(array3));
+
+string[] primerInput = InputArr();
+PrintResult(primerInput, SelectItem(primerInput));
