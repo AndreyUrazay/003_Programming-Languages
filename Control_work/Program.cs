a6dcc2a55728117d865ea174c98037d40d98d61c
Andrey Urazay <urazay.andrey@mail.ru>
2023-02-12 18:46:47 +0700

    Control_work добавили метод ввода

---
+++ Control_work/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] InputArr(char x = ' ')
+{
+
+    Console.Write($"Для выполнения алгоритма введите символы через пробел {x}: ");
+    string symbol = Console.ReadLine()?? " ";
+    return symbol.Split(x);
+}
+
+
